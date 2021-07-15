053e3c5472e446f7e0967b2cc1713708da0f0f8b
Jan Schumann <js@schumann-it.com>
2021-07-15 21:22:29 +0200

    support terraform >= 1

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = "~> 0.13"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws = "~> 3.2"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.2"
+    }
   }
 }
